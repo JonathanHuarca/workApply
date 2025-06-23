5e6fa5da80bbfdbba4efc69e2c5b5099353ade15
JonathanHuarca <jonathan.huarca@tecsup.edu.pe>
2025-06-22 23:10:59 -0500

    add: getAllReviews, addReviews, getReviewById and DeleteReviewById

--- workApply/src/main/java/com/steve/workApply/review/ReviewController.java
+++ workApply/src/main/java/com/steve/workApply/review/ReviewController.java
@@ -2,10 +2,7 @@ package com.steve.workApply.review;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,46 @@ public class ReviewController {
     public ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId){
         return new ResponseEntity<>(reviewService.getAllReviews(companyId),
                 HttpStatus.OK);
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<String> addReviews(@PathVariable Long companyId, @RequestBody Review review){
+        boolean reviewAdd = reviewService.addReview(companyId, review);
+        if(reviewAdd){
+            return new ResponseEntity<>("Review Added Successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/reviews/{reviewId}")
+    public ResponseEntity<Review> getReview(@PathVariable Long companyId,
+                                                @PathVariable Long reviewId){
+        Review review = reviewService.getReview(companyId,reviewId);
+        if(review != null ){
+            return new ResponseEntity<>(review, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable Long companyId,
+                                               @PathVariable Long reviewId,
+                                               @RequestBody Review review){
+        boolean isReviewUpdate = reviewService.updateReview(companyId, reviewId, review);
+        if(isReviewUpdate)
+            return new ResponseEntity<>("Review update Successfully", HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable Long companyId,
+                                               @PathVariable Long reviewId){
+
+        boolean isReviewUpdate = reviewService.deleteReview(companyId, reviewId);
+        if(isReviewUpdate)
+            return new ResponseEntity<>("Review deteled Successfully", HttpStatus.OK);
+        else
+            return new ResponseEntity<>("Review deteled NotFound", HttpStatus.NOT_FOUND);
     }
 }
--- workApply/src/main/java/com/steve/workApply/review/impl/ReviewServiceImpl.java
+++ workApply/src/main/java/com/steve/workApply/review/impl/ReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.steve.workApply.review.impl;
 
+import com.steve.workApply.company.Company;
+import com.steve.workApply.company.CompanyService;
 import com.steve.workApply.review.Review;
 import com.steve.workApply.review.ReviewRepository;
 import com.steve.workApply.review.ReviewService;
@@ -10,15 +12,66 @@ import java.util.List;
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
+    private final CompanyService companyService;
 
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
+    public ReviewServiceImpl(ReviewRepository reviewRepository,
+                             CompanyService companyService) {
         this.reviewRepository = reviewRepository;
+        this.companyService = companyService;
     }
 
     @Override
     public List<Review> getAllReviews(Long companyId) {
         List<Review> reviews = reviewRepository.findByCompanyId(companyId);
         return reviews;
+    }
+
+    @Override
+    public boolean addReview(Long companyId, Review review) {
+        Company company = companyService.getCompanyById(companyId);
+        if(company != null){
+            review.setCompany(company);
+            reviewRepository.save(review);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Review getReview(Long companyId, Long reviewId) {
+        List<Review> reviews = reviewRepository.findByCompanyId(companyId);
+        return reviews.stream()
+                .filter(review -> review.getId().equals(reviewId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public boolean updateReview(Long companyId, Long reviewId, Review updateReview) {
+        if(companyService.getCompanyById(companyId) != null){
+            updateReview.setCompany(companyService.getCompanyById(companyId));
+            updateReview.setId(reviewId);
+            reviewRepository.save(updateReview);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteReview(Long companyId, Long reviewId) {
+        if(companyService.getCompanyById(companyId) != null
+                && reviewRepository.existsById(reviewId)){
+            Review review = reviewRepository.findById(reviewId).orElse(null);
+            Company company = review.getCompany();
+            company.getReviews().remove(review);
+            review.setCompany(null);
+            companyService.updateCompany(company, companyId);
+            reviewRepository.deleteById(reviewId);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
